4fadf25004af9f912086fcb5ff91bb2d85133a73
gabyzif <gabyzif@gmail.com>
2023-05-14 15:07:40 -0300

    wip virtualization + fix styles

--- src/components/Cards/CardContainer.tsx
+++ src/components/Cards/CardContainer.tsx
@@ -2,6 +2,7 @@
 import { useEffect, useRef } from 'react';
 import Card from './Card';
 import useSWRInfinite from 'swr/infinite';
+import { FixedSizeGrid as Grid } from 'react-window';
 
 const CardContainer = () => {
   const fetcher = (url) => fetch(url).then((res) => res.json());
@@ -29,17 +30,51 @@ const CardContainer = () => {
     window.addEventListener('scroll', handleScroll);
     return () => window.removeEventListener('scroll', handleScroll);
   }, [setSize, size]);
-
   return (
-    <div className="card-container grid md:gap-3 md:grid-cols-4" ref={containerRef}>
-      {nfts.length > 0 &&
-        nfts.map(({ results }) =>
-          results.map((r) => <Card key={r.name} price={r.price} title={r.title} imageSrc={r.img} />)
-        )}
-
-      {isLoadingMore && <div>Loading...</div>}
-      {isReachingEnd && <div>End of results</div>}
-    </div>
+    <>
+      <div className="card-container grid md:gap-3 md:grid-cols-4" ref={containerRef}>
+        {nfts.length > 0 &&
+          nfts.map(({ results }) =>
+            results.map((r) => <Card key={r.name} price={r.price} title={r.title} imageSrc={r.img} />)
+          )}
+      </div>
+      {isLoadingMore ? (
+        <div className="flex my-10 justify-center">
+          <button
+            type="button"
+            className="flex items-center rounded-lg bg-tertiary-regular p-4  text-white"
+            disabled
+          >
+            <svg
+              className="mr-3 h-5 w-5 animate-spin text-white"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                stroke-width="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
+            </svg>
+            <span className="font-medium"> Loading ... </span>
+          </button>
+        </div>
+      ) : null}
+      {isReachingEnd ? (
+        <div>
+          <p className="text-white ">End of results</p>
+        </div>
+      ) : null}
+    </>
   );
 };
 
